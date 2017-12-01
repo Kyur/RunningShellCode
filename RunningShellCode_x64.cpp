84a7e7053f4d547e5b40e6691a74bd52bf673d56
Kur <kkw90111@naver.com>
2017-12-01 17:18:37 +0900

    RunningShellCode_64.cpp 20171201

--- RunningShellCode_x64.cpp
+++ RunningShellCode_x64.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
 
-#define ERROR -1
-#define GOOD 1
-
+//	Function pointer for shell code
 typedef void (*FNPTR)();
 
 int main(int argc, char** argv)
@@ -11,46 +9,62 @@ int main(int argc, char** argv)
 	char* shellCodeFileName;
 	HANDLE hShellCode = NULL;
 	unsigned int fileSize = 0;
+	unsigned int realAllocSize = 0;
 	PVOID pAllocMem = NULL;
 	DWORD NumberOfBytesRead = 0;
 	FNPTR fPtr = NULL;
 
 	if(argc != 2)
 	{
-		printf(" [!] Need argument.\n");
-		return ERROR;
+		printf(" [!] Usage : RunningShellCode_x64.exe ShellCodeFile\n");
+		return -1;
 	}
 
 	shellCodeFileName = argv[1];
 	hShellCode = CreateFile(shellCodeFileName, GENERIC_ALL, NULL, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if(!hShellCode)
 	{
-		printf(" [!] Fail CreateFile\n");
-		return ERROR;
+		printf(" [!] Fail : Fail to CreateFile().\n");
+		return -1;
 	}
 
 	fileSize = GetFileSize(hShellCode, NULL);
-	if(!fileSize)
+	if(fileSize <= 0)
 	{
-		printf(" [!] File size error\n");
-		return ERROR;
+		printf(" [!] Fail : Abnormal file size.\n");
+		return -1;
 	}
 
-	pAllocMem = VirtualAlloc(NULL, fileSize + 0x1000, MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	//	+ 0x1000 Extra size to allocate memory.
+	realAllocSize = fileSize + 0x1000;
+
+	pAllocMem = (PVOID)VirtualAlloc(NULL, realAllocSize, MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 	if(!pAllocMem)
 	{
-		printf(" [!] VirtualAlloc ERROR.\n");
-		return ERROR;
+		printf(" [!] Fail : Fail to VirtualAlloc().\n");
+		return -1;
 	}
 
-	ReadFile(hShellCode, pAllocMem, fileSize, &NumberOfBytesRead, NULL);
+	//	Set 0x90 to memory area.
+	memset(pAllocMem, 0x90, realAllocSize);
+	//	Set 0xCC to memory start.
+	memset(pAllocMem, 0xCC, 0x01);
+
+	//	Read shell code to allocated memory.
+	if(!ReadFile(hShellCode, ((PBYTE)pAllocMem + 1), fileSize, &NumberOfBytesRead, NULL))
+	{
+		printf(" [!] Fail : Fail to ReadFile().\n");
+		return -1;
+	}
 	
-	fPtr = (FNPTR) pAllocMem;
-	OutputDebugString("===== INTO HERE ====");
+	fPtr = (FNPTR)pAllocMem;
+
+	//	Call allocated memory area.
 	fPtr();
 
-	VirtualFree(pAllocMem, fileSize + 0x1000, MEM_RELEASE);
+	VirtualFree(pAllocMem, realAllocSize, MEM_RELEASE);
 	CloseHandle(hShellCode);
 	
-	return GOOD;
+	return 1;
 }
+
